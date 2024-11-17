2975d53331d204b9a2b52bca942d5487697c2202
Masha Badouskaya <mashabodowskaya@gmail.com>
2024-11-18 00:36:21 +0100

    First Commit with my homework (without * task)

---
+++ base/test_example.py
@@ -0,0 +1,44 @@
+import pytest
+import random
+
+
+@pytest.fixture
+def foo():
+    my_list = [random.randint(1, 50) for i in range(10)]
+    print(my_list)
+    print(sorted(my_list))
+    print(len(my_list))
+    print(sum(my_list))
+    return my_list
+
+
+@pytest.mark.length
+def test_length(foo):
+    assert len(foo) == 10
+
+
+@pytest.mark.sort
+def test_sort(foo):
+    assert sorted(foo) == sorted(foo)
+
+
+@pytest.mark.range
+def test_range(foo):
+    for i in foo:
+        assert 1 <= i <= 50
+
+
+@pytest.mark.sum
+def test_sum(foo):
+    assert sum(foo) > 0
+
+
+@pytest.mark.odds_evens
+def test_odds(foo):
+    assert any(i % 2 != 0 for i in foo)
+
+
+@pytest.mark.odds_evens
+def test_evens(foo):
+    assert any(i % 2 == 0 for i in foo)
+
